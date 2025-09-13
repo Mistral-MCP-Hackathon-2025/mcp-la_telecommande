4803a1ce1b751177a790ac402f27fbf3cff02284
Armand BLIN <armand.blin@gmail.com>
2025-09-13 16:22:28 +0200

    fix(config): mcp no stateless

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from mcp.server.fastmcp import FastMCP
 
 # Create the MCP server
-mcp = FastMCP("SSH_MCP", port=3000, debug=True)
+mcp = FastMCP("SSH_MCP", port=3000, debug=True, stateless_http=True)
 
 # ruff: noqa: F401, E402
 import src.SSH
